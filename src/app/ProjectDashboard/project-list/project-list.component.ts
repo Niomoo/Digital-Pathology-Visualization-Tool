14ec0a318d569c473d02260760d77228e0581f31
Niomoo <jennyliu.lyh@gmail.com>
2023-04-02 21:43:07 +0800

    [Feature]Add dashsboard sample project

--- src/app/ProjectDashboard/project-list/project-list.component.ts
+++ src/app/ProjectDashboard/project-list/project-list.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent {
-
+  projectList = [
+    { Title: "LUAD", Type: "WSI images"},
+    { Title: "BRCA", Type: "WSI images"},
+    { Title: "UCEC", Type: "WSI images"},
+    { Title: "GBM", Type: "WSI images"},
+    { Title: "KIRC", Type: "WSI images"},
+  ];
 }
+
