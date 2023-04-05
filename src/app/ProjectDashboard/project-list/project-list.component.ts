e04b99ab668310c1c4826e106069e19731059590
niomoo <jennyliu.lyh@gmail.com>
2023-04-06 03:57:27 +0800

    [Test]Modify the error handling display

--- src/app/ProjectDashboard/project-list/project-list.component.ts
+++ src/app/ProjectDashboard/project-list/project-list.component.ts
@@ -24,6 +24,32 @@ export class ProjectListComponent implements OnInit {
         this.projectData = data;
       },
       error: (error) => {
+        this.projectData = [
+          {
+            'title': 'BRCA',
+            'type': 'WSI images',
+          },
+          {
+            'title': 'LUAD',
+            'type': 'WSI images',
+          },
+          {
+            'title': 'KIRC',
+            'type': 'WSI images',
+          },
+          {
+            'title': 'UCEC',
+            'type': 'WSI images',
+          },
+          {
+            'title': 'GBM',
+            'type': 'WSI images',
+          },
+          {
+            'title': 'HNSC',
+            'type': 'WSI images',
+          },
+        ];
         console.log(error);
       }
     })
