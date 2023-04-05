fa932d5b4adaf227543257040caa0b8a54941b95
Niomoo <jennyliu.lyh@gmail.com>
2023-04-06 03:13:00 +0800

    [Feature]Add first judgement page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
     extend: {
       backgroundImage: {
         'main': "url('src/assets/background.svg')",
+      },
+      colors: {
+        'primary': "#F6F8FD",
+      },
+      gridTemplateRows: {
+        '12': "repeat(12, minmax(0, 1fr))",
+      },
+      gridRow: {
+        'span-8': 'span 8 / span 8',
       }
     },
   },
